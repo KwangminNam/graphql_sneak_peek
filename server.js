beb11b5fc0d01d8771bd3bc709e81179426791b8
kwangminNam <johnpower@naver.com>
2023-07-20 23:09:17 +0900

    add single api request

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 import { ApolloServer, gql } from "apollo-server";
-import fetch from 'node-fetch';
+import fetch from "node-fetch";
+
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5MDJkY2VjZjc5M2VhZDljMTgxM2FjOTFkNTVlODMzMyIsInN1YiI6IjYzMDEwMTQxYjM5ZTM1MDA4MmQ1YTI5OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.4rzqk8JLgFsvNVb3qfyunf1Uyf1mAgbGCcDocYofMjg"
+  }
+};
 
 let db = [
   {
@@ -11,34 +20,35 @@ let db = [
     id: "",
     text: "22hello",
     userId: "2"
-  },
-]
+  }
+];
 let user = [
-  { id: "1", firstName: 'km', lastName: 'nam' },
-  { id: "2", firstName: 'km', lastName: 'shin' },
-]
+  { id: "1", firstName: "km", lastName: "nam" },
+  { id: "2", firstName: "km", lastName: "shin" }
+];
 
 const typeDefs = gql`
-  type User{
-    id:ID!
-    firstName:String!
-    lastName:String
-    fullName:String!
+  type User {
+    id: ID!
+    firstName: String!
+    lastName: String
+    fullName: String!
   }
   type Tweet {
-    id:ID!
-    text:String!
-    author:User
+    id: ID!
+    text: String!
+    author: User
   }
   type Query {
-    allMovies:[Movie!]!
-    allTweets:[Tweet!]!
-    tweet(id:ID!):Tweet
-    getUser:[User]
+    allMovies: [Movie!]!
+    allTweets: [Tweet!]!
+    tweet(id: ID!): Tweet
+    getSingleMovie(id: ID!): SingleMovie
+    getUser: [User]
   }
   type Mutation {
-    postTweet(text:String!, userId:ID!):Tweet!
-    deleteTweet(id:ID!):Boolean!
+    postTweet(text: String!, userId: ID!): Tweet!
+    deleteTweet(id: ID!): Boolean!
   }
   type Movie {
     adult: Boolean
@@ -56,6 +66,29 @@ const typeDefs = gql`
     vote_count: Int
     genre_ids: [Int]
   }
+  type SingleMovie {
+    adult: Boolean
+    backdrop_path: String
+    budget: Int
+    homepage: String
+    id: Int
+    imdb_id: String
+    original_language: String
+    original_title: String
+    overview: String
+    popularity: Float
+    poster_path: String
+    release_date: String
+    revenue: Int
+    runtime: Int
+    status: String
+    tagline: String
+    title: String
+    video: Boolean
+    vote_average: Float
+    vote_count: Int
+
+  }
 `;
 
 const resolvers = {
@@ -64,13 +97,24 @@ const resolvers = {
       return db;
     },
     tweet(root, { id }) {
-      return db.find(item => item.id == id);
+      return db.find((item) => item.id == id);
     },
     getUser() {
       return user;
     },
     allMovies() {
-      return fetch("https://api.themoviedb.org/3/movie/now_playing?api_key=10923b261ba94d897ac6b81148314a3f").then(res => res.json()).then(json => json.results)
+      return fetch(
+        "https://api.themoviedb.org/3/movie/now_playing?api_key=10923b261ba94d897ac6b81148314a3f"
+      )
+        .then((res) => res.json())
+        .then((json) => json.results);
+    },
+    getSingleMovie(__, { id }) {
+      const url = `https://api.themoviedb.org/3/movie/${id}`;
+      return fetch(url, options)
+        .then((res) => res.json())
+        .then((json) => json)
+        .catch((err) => console.error("error:" + err));
     }
   },
   Mutation: {
@@ -85,29 +129,29 @@ const resolvers = {
       return newData;
     },
     deleteTweet(__, { id }) {
-      const findTweet = db.find(item => item.id === id);
+      const findTweet = db.find((item) => item.id === id);
       if (!findTweet) return false;
-      db = db.filter(item => item.id !== findTweet.id);
+      db = db.filter((item) => item.id !== findTweet.id);
       return true;
     }
   },
   User: {
     fullName({ id, firstName, lastName }) {
-      return `${firstName}${lastName}`
+      return `${firstName}${lastName}`;
     }
   },
   Tweet: {
     author({ userId }) {
-      const result = user.find(item => item.id === userId);
+      const result = user.find((item) => item.id === userId);
       console.log(result);
       if (!result) console.log("NOPE");
       return result;
     }
   }
-}
+};
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen().then(({ url }) => {
-  console.log('running' + url);
-})
+  console.log("running" + url);
+});
