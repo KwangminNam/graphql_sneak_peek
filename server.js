46d5994978ea5261e03d8ba286f1ca698d4e9522
kwangminNam <johnpower@naver.com>
2023-07-20 15:17:28 +0900

    REST API to GraphQL

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
-import { ApolloServer,gql } from "apollo-server";
+import { ApolloServer, gql } from "apollo-server";
+import fetch from 'node-fetch';
 
-
-let db =[
+let db = [
   {
-    id:"1",
-    text:"11hello",
+    id: "1",
+    text: "11hello",
+    userId: "1"
   },
   {
-    id:"2",
-    text:"22hello",
+    id: "",
+    text: "22hello",
+    userId: "2"
   },
 ]
 let user = [
-  {id:"1" ,firstName:'km',lastName:'nam'}
+  { id: "1", firstName: 'km', lastName: 'nam' },
+  { id: "2", firstName: 'km', lastName: 'shin' },
 ]
 
 const typeDefs = gql`
@@ -20,6 +23,7 @@ const typeDefs = gql`
     id:ID!
     firstName:String!
     lastName:String
+    fullName:String!
   }
   type Tweet {
     id:ID!
@@ -27,6 +31,7 @@ const typeDefs = gql`
     author:User
   }
   type Query {
+    allMovies:[Movie!]!
     allTweets:[Tweet!]!
     tweet(id:ID!):Tweet
     getUser:[User]
@@ -35,37 +40,74 @@ const typeDefs = gql`
     postTweet(text:String!, userId:ID!):Tweet!
     deleteTweet(id:ID!):Boolean!
   }
+  type Movie {
+    adult: Boolean
+    backdrop_path: String
+    id: Int
+    original_language: String
+    original_title: String
+    overview: String
+    popularity: Float
+    poster_path: String
+    release_date: String
+    title: String
+    video: Boolean
+    vote_average: Float
+    vote_count: Int
+    genre_ids: [Int]
+  }
 `;
 
 const resolvers = {
-  Query : {
-    allTweets(){
+  Query: {
+    allTweets() {
       return db;
     },
-    tweet(root,{id}){
+    tweet(root, { id }) {
       return db.find(item => item.id == id);
     },
-    getUser(){
+    getUser() {
       return user;
+    },
+    allMovies() {
+      return fetch("https://api.themoviedb.org/3/movie/now_playing?api_key=10923b261ba94d897ac6b81148314a3f").then(res => res.json()).then(json => json.results)
     }
   },
-  Mutation:{
-    postTweet(__,{text,userId}){
-      const newData = {id:db.length + 1,text};
+  Mutation: {
+    postTweet(__, { text, userId }) {
+      console.log(userId);
+      const newData = {
+        id: db.length + 1,
+        text,
+        userId
+      };
       db.push(newData);
       return newData;
     },
-    deleteTweet(__,{id}){
+    deleteTweet(__, { id }) {
       const findTweet = db.find(item => item.id === id);
-      if(!findTweet) return false;
+      if (!findTweet) return false;
       db = db.filter(item => item.id !== findTweet.id);
       return true;
+    }
+  },
+  User: {
+    fullName({ id, firstName, lastName }) {
+      return `${firstName}${lastName}`
+    }
+  },
+  Tweet: {
+    author({ userId }) {
+      const result = user.find(item => item.id === userId);
+      console.log(result);
+      if (!result) console.log("NOPE");
+      return result;
     }
   }
 }
 
-const server = new ApolloServer({typeDefs,resolvers});
+const server = new ApolloServer({ typeDefs, resolvers });
 
-server.listen().then(({url})=>{
-  console.log('running'+url);
+server.listen().then(({ url }) => {
+  console.log('running' + url);
 })
